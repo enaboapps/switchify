8fd5c51c1eabadcd0402018e58bd39751b1396dd
OwenMcGirr <o.a.mcgirr@gmail.com>
2025-01-05 11:51:54 +0000

    Add purchase confirmation screen
    closes #877

--- app/src/main/java/com/enaboapps/switchify/screens/paywall/AppPaywallScreen.kt
+++ app/src/main/java/com/enaboapps/switchify/screens/paywall/AppPaywallScreen.kt
@@ -1,7 +1,17 @@
 package com.enaboapps.switchify.screens.paywall
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.enaboapps.switchify.components.BaseView
+import com.enaboapps.switchify.components.FullWidthButton
 import com.enaboapps.switchify.screens.paywall.model.AppPaywallScreenModel
 import com.revenuecat.purchases.ui.revenuecatui.ExperimentalPreviewRevenueCatUIPurchasesAPI
 import com.revenuecat.purchases.ui.revenuecatui.PaywallDialog
@@ -11,9 +21,34 @@ import com.revenuecat.purchases.ui.revenuecatui.PaywallDialogOptions
 @Composable
 fun AppPaywallScreen(navController: NavController) {
     val model = AppPaywallScreenModel()
+    val showingConfirmationState = model.showingConfirmation.observeAsState()
     val options = PaywallDialogOptions.Builder()
         .setListener(model)
         .setDismissRequest { navController.popBackStack() }
         .build()
-    PaywallDialog(options)
+
+    if (showingConfirmationState.value == true) {
+        BaseView(
+            title = "Switchify Pro Is Yours",
+            navController = navController,
+            enableScroll = false,
+            padding = 25.dp
+        ) {
+            Text(
+                text = "Thank you for supporting Switchify! You can now access all Switchify features.",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.padding(horizontal = 20.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            FullWidthButton(
+                text = "Done",
+                onClick = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    } else {
+        PaywallDialog(options)
+    }
 }
