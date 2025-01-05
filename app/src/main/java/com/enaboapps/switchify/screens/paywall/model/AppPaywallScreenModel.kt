ad1f2d723d6d4a59b8961856696472e773a3f6ec
OwenMcGirr <o.a.mcgirr@gmail.com>
2025-01-05 17:47:53 +0000

    Different approach to displaying purchase confirmation
    closes #880

--- app/src/main/java/com/enaboapps/switchify/screens/paywall/model/AppPaywallScreenModel.kt
+++ app/src/main/java/com/enaboapps/switchify/screens/paywall/model/AppPaywallScreenModel.kt
@@ -1,6 +1,5 @@
 package com.enaboapps.switchify.screens.paywall.model
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.enaboapps.switchify.backend.iap.IAPHandler
 import com.revenuecat.purchases.CustomerInfo
@@ -8,15 +7,11 @@ import com.revenuecat.purchases.models.StoreTransaction
 import com.revenuecat.purchases.ui.revenuecatui.PaywallListener
 
 class AppPaywallScreenModel : ViewModel(), PaywallListener {
-    var showingConfirmation = MutableLiveData(false)
-
     override fun onRestoreCompleted(customerInfo: CustomerInfo) {
         super.onRestoreCompleted(customerInfo)
 
         // Handle restore completion
         IAPHandler.refreshPurchaseStatus()
-
-        showingConfirmation.value = true
     }
 
     override fun onPurchaseCompleted(
@@ -27,7 +22,5 @@ class AppPaywallScreenModel : ViewModel(), PaywallListener {
 
         // Handle purchase completion
         IAPHandler.refreshPurchaseStatus()
-
-        showingConfirmation.value = false
     }
 }
